35bd6888b6efd6b77ad03194e15c7e1be3a9e669
GabrielCerveira <gabrieloliveira033@outlook.com>
2022-11-11 16:11:17 -0300

    pbinput

---
+++ src/components/generic/PBInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-input
+   :model="value"
+   :label="label"
+   :type="[type !== 'password' ? type : isPwd ? 'password' : 'text']"
+  >
+    <template v-slot:append>
+       <!--Templeate de senha-->
+      <q-icon
+        v-if="type === 'password'"
+        :name="isPwd ? 'visibility_off' : 'visibility'"
+        class="cursor-pointer"
+        @click="isPwd = !isPwd"
+      />
+
+      <!--Templeate de data-->
+      <q-icon v-if="type === 'date'" name="event" class="cursor-pointer">
+        <q-popup-proxy transition-show="scale" transition-hide="scale">
+          <q-date v-model="date">
+            <div class="row items-center justify-end">
+               <q-btn v-close-popup label="Close" color="primary" flat />
+            </div>
+          </q-date>
+        </q-popup-proxy>
+      </q-icon>
+    </template>
+  </q-input>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "PBinput",
+  props: {
+    value:{
+      type: String
+    },
+    type:{
+      type: String,
+      default: 'text'
+    },
+    label:{
+      type:String
+    }
+  },
+  setup(){
+    return{
+      isPwd: ref(true)
+    }
+  }
+})
+</script>
+
+<style>
+
+
+</style>
--- src/pages/authentication/LoginPage.vue
+++ src/pages/authentication/LoginPage.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="row q-pa-xl">
-    <div class="col-sm-5 col-md-8 gt-xs">
-      conteudo
-    </div>
+  <div class="row q-pa-xl window-height">
+    <div class="col-sm-5 col-md-8 gt-xs">conteudo</div>
     <div class="col-xs-12 col-sm-7 col-md-4 authentication">
-      <div class="q-pa-md q-gutter-md authentication-login">
+      <div class="q-pa-md q-gutter-y-md authentication-login">
         <div class="col-12 text-center">
           <span>Predict Ball</span>
         </div>
         <div>
-          <q-input class="col-12"  placeholder="Insira o seu e-mail"> </q-input>
-          <q-input placeholder="Senha" type="password"> </q-input>
+          <PB-input :label="'Email'" :type="'email'" v-model="email"/>
+          <PB-input :label="'Senha'" :type="'password'" v-model="email"/>
+          <PB-input :label="'Data'" :type="'date'" v-model="email"/>
         </div>
         <div class="q-mb-lg">
-          <q-btn label="Entrar" class="full-width"></q-btn>
+          <q-btn
+            label="Entrar"
+            class="full-width"
+          />
         </div>
-        <q-separator size="1px"/>
+        <q-separator size="1px" />
         <div class="q-mt-lg">
-          <q-btn icon="mdi-google-plus" label="Entrar com Google" class="full-width"></q-btn>
-          <q-btn flat label="Esqueceu a senha?" class="full-width"></q-btn>
+          <q-btn
+            icon="mdi-google-plus"
+            label="Entrar com Google"
+            class="full-width"
+          />
+          <q-btn
+            flat
+            label="Esqueceu a senha?"
+            class="full-width"
+          />
         </div>
       </div>
       <div class="q-pa-md q-my-lg text-center authentication-create">
@@ -29,20 +39,26 @@
 </template>
 
 <script>
-import { defineComponent } from "vue"
+import PBInput from "src/components/generic/PBInput.vue"
+import { defineComponent, ref } from "vue"
 
 export default defineComponent({
+  components: { PBInput },
   name: "LoginPage",
+  setup(){
+    return{
+      email: ref('')
+    }
+  }
 })
 </script>
 
 <style lang="scss" scoped>
-.authentication-login{
+.authentication-login {
   border: 1px solid rgba(17, 17, 17, 0.137);
 }
 
-.authentication-create{
+.authentication-create {
   border: 1px solid rgba(17, 17, 17, 0.137);
 }
 </style>
-
